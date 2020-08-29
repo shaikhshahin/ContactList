14204807d5cd28d4341adfd9c31e7d21abaa0c50
Shahin Shaikh <shaikhshahin2909@gmail.com>
2020-08-29 21:10:18 +0400

    First Commit

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as contactAction from './src/actions/contactAction';
-
+//
 class App extends Component {
 
   constructor(props){
