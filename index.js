4b3ba6683a380d8c883fcfc3896512c617cfc1f8
Shahin Shaikh <shaikhshahin2909@gmail.com>
2020-08-29 21:00:40 +0400

    First Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * @format
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Provider } from 'react-redux';
+import configureStore from './src/store/configureStore';
+
+const store = configureStore();
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>, document.getElementById('root'));
+
